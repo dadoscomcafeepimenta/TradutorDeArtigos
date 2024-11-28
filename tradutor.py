e85b79107420c4e096409d6959ea6787f7b1b046
dadoscomcafeepimenta <dadoscomcafeepimenta@gmail.com>
2024-11-28 14:46:24 -0300

    Initial commit

---
+++ tradutor.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from langchain_openai.chat_models.azure import AzureChatOpenAI
+
+def extract_text_from_url(url):
+	response = requests.get(url)
+
+	if response.status_code == 200:
+		soup = BeautifulSoup(response.text, 'html.parser')
+
+		for script_or_style in soup(["script", "style"]):
+			script_or_style.decompose()
+
+		texto = soup.get_text(separator=' ')
+		# limpar texto
+		linhas = (line.strip() for line in texto.splitlines())
+		parts = (phrase.strip() for line in linhas for phrase in line.split(" "))
+		texto_limpo = '\n'.join(part for part in parts if part)
+		texto_limpo = texto_limpo[:1000]
+		return texto_limpo
+	else:
+		print(f"Failed to fetch URL. Status code: {response.status_code}")
+		return None
+
+client = AzureChatOpenAI(
+	azure_endpoint="https://wanessa-oai-dio-bootcamp-dev-eastus-001.openai.azure.com/",
+	api_key="6aeOOmDSRWVLheeZ3VH6V82xhP4eh4P0swMvfjlzNcxvK11lqOULJQQJ99AKACHYHv6XJ3w3AAABACOGIKfn",
+	api_version="2024-02-15-preview",
+	deployment_name="gpt-4o-mini",
+	max_retries=0
+)
+
+def translate_article(text, lang):
+	messages = [
+		("system", "Você atua como tradutor de textos"),
+		("user", f"Traduza o {text} para o idioma {lang} e responda em markdown")
+	]
+
+	response = client.invoke(messages)
+	print(response.content)
+	return response.content
+
+url = "https://dev.to/kenakamu/azure-open-ai-in-vnet-3alo"
+text = extract_text_from_url(url)
+article = translate_article(text, "pt-br")
+
+print(article)
